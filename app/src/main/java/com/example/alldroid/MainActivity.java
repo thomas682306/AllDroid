04539c84ed89e9fbd401bc75bd417b2b4cc80c5c
thomas682306 <43097532+thomas682306@users.noreply.github.com>
2020-05-15 23:55:18 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/alldroid/MainActivity.java
+++ app/src/main/java/com/example/alldroid/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //why is this is not always working
     }
 }
