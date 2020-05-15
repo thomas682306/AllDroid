7e867c8812670a337b7d3a431c2fbca6a8e0f658
thomas682306 <43097532+thomas682306@users.noreply.github.com>
2020-05-16 00:05:02 +0530

    added a log

--- app/src/main/java/com/example/alldroid/MainActivity.java
+++ app/src/main/java/com/example/alldroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.alldroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //why is this is not always working
+
+
+        Log.d("Thomas","Im finally getting the hang of this");
+
+
     }
 }
