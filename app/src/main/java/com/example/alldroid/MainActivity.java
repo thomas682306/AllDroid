ecb9f8da7c4db7db8e6dc03354d9590a9d1ae3de
thomas682306 <43097532+thomas682306@users.noreply.github.com>
2020-05-15 22:13:49 +0530

    added assets folder

--- app/src/main/java/com/example/alldroid/MainActivity.java
+++ app/src/main/java/com/example/alldroid/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Pani pali mwonuse avaracha", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "heyehyehyeyeeyehyeh", Toast.LENGTH_SHORT).show();
+
     }
 }
