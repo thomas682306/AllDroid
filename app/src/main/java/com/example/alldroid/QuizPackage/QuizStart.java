7c387f5aa1f8a5d5c73d723bb296727c3f432856
thomas682306 <43097532+thomas682306@users.noreply.github.com>
2020-05-17 16:23:31 +0530

    Finished all UI components of quiz

--- app/src/main/java/com/example/alldroid/QuizPackage/QuizStart.java
+++ app/src/main/java/com/example/alldroid/QuizPackage/QuizStart.java
@@ -1,8 +1,13 @@
 package com.example.alldroid.QuizPackage;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +16,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.alldroid.R;
-public class QuizStart extends Fragment {
-    ProgressBar mProgressBar;
-    TextView startFeedbackText;
+import com.google.android.material.button.MaterialButton;
 
+import static androidx.navigation.fragment.NavHostFragment.findNavController;
+
+public class QuizStart extends Fragment {
+    com.google.android.material.button.MaterialButton readyButton;
+    NavController navController;
     public QuizStart() {
         // Required empty public constructor
     }
@@ -28,6 +36,24 @@ public class QuizStart extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_quiz_start, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        readyButton=view.findViewById(R.id.ready_button);
+        navController = Navigation.findNavController(view);
+
+        readyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            navController.navigate(R.id.action_quizStart_to_mainQuizFragment);
+
+            }
+        });
+    }
+
 }
